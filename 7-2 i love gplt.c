049243fc8cf93b92117ae2888ff7e0c5ab315c9a
liwangandwangli <114417882+liwangandwangli@users.noreply.github.com>
2022-10-11 23:25:48 +0800

    Add files via upload

---
+++ 7-2 i love gplt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("I\n");
+	printf(" \n");
+	printf("L\n");
+	printf("O\n");
+	printf("V\n");
+	printf("E\n");
+	printf(" \n");
+	printf("G\n");
+	printf("P\n");
+	printf("L\n");
+	printf("T\n");
+	return 0;
+									
+}
